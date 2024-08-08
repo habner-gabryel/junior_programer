dd5d959d7206e40948c13a59a35be2dcd23f569b
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-08-08 12:38:04 -0300

    4.7 commit

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -26,4 +26,8 @@ public class MenuUIHandler : MonoBehaviour
     public void StartNew() {
         SceneManager.LoadScene(1);
     }
+
+    public void Exit() {
+        Application.Quit();
+    }
 }

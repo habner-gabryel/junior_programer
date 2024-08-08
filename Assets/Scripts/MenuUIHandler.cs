9bd2b16ca65c20d0326c9d368e0a9c1900ad2d3a
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-08-08 13:03:33 -0300

    4.8 commit

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 // Sets the script to be executed later than all default scripts
 // This is helpful for UI, since other things may need to be initialized before setting the UI
 [DefaultExecutionOrder(1000)]
@@ -28,6 +29,10 @@ public class MenuUIHandler : MonoBehaviour
     }
 
     public void Exit() {
-        Application.Quit();
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit();
+        #endif
     }
 }

754aa23651b99884f39c606224ae75389ce60e2c
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-08-12 22:06:42 -0300

    6.8 commit

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -19,12 +19,11 @@ public class MainManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
    }
 
-}
-
-[System.Serializable]
-class SaveData
-{
-    public Color TeamColor;
+    [System.Serializable]
+    class SaveData
+    {
+        public Color TeamColor;
+    }
 
     public void SaveColor()
     {
@@ -32,7 +31,19 @@ class SaveData
         data.TeamColor = TeamColor;
 
         string json = JsonUtility.ToJson(data);
-    
+
         File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            TeamColor = data.TeamColor;
+        }
     }
 }

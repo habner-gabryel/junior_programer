94d8fb7f0b4b49c3c83c87f35746f51ef71772a4
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-08-19 18:35:47 -0300

    refactoring 2.4

---
+++ Assets/Scripts/ProductivityUnit.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductivityUnit : Unit
+{
+    protected override void BuildingInRange(){
+
+    }
+}
